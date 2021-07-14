e52d92ec75a48ea838976980933bdd3e7398afe2
Subham <paras123hzb@gmail.com>
2021-07-14 16:03:16 +0530

    add

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const PORT = process.env.PORT || 5000
 const {MONGOURI} = require('./config/keys')
 
 var cors = require('cors')
-app.use(cors())
+app.use(cors({
+    origin:"*",
+}))
 
 mongoose.connect(MONGOURI,{
     useNewUrlParser:true,
